494387fd5a2b79230cf4a83f8525b773c0e02392
thuan <lapthuan0805@gmail.com>
2023-03-04 14:57:43 +0700

    first commit

---
+++ routes/middlewareController.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const middlewareController = {
+  verifyToken: (req, res, next) => {
+    const token = req.headers.token;
+    if (token) {
+      const accessToken = token.split(" ")[1];
+      jwt.verify(accessToken, process.env.ACCESS_TOKEN_KEY, (err, user) => {
+        if (err) {
+          return res.status(403).json("Token is not valid");
+        }
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(401).json("You're not authenticated");
+    }
+  },
+};
+
+module.exports = middlewareController;
